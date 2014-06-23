0cf315022179011bef64272369e3cf17ff96504f
andre.moreira <andre.moreira@cwi.com.br>
2014-06-23 17:28:26 -0300

    Novos métodos de busca

--- Push/DeviceManager.php
+++ Push/DeviceManager.php
@@ -24,7 +24,8 @@ class DeviceManager {
 	 */
 	public static function validateDevice($device, $validateDeviceType = true) {
 		// verifica se o identificador de push foi informado
-		if (!$device->getToken()) {
+		$token = $device->getToken();
+		if (!$token || !is_string($token)) {
 			throw new \InvalidArgumentException("O identificador do dispositivo não foi informado.");
 		}
 
@@ -36,24 +37,101 @@ class DeviceManager {
 
 		// verifica o tipo foi informado e é uns dos tipos válidos
 		$type = $device->getDeviceType();
-		if ($validateDeviceType && (!$type || ($type != Device::ANDROID && $type != Device::IOS))) {
+		if ($validateDeviceType && (!$type || ($type !== Device::ANDROID && $type !== Device::IOS))) {
 			throw new \InvalidArgumentException("O tipo do dispositivo não foi informado ou é inválido.");
+		}
+	}
+
+	/**
+	 * Busca todos os dispositivos
+	 *
+	 * @param int $page Página a ser retornada
+	 * @param int $limit Limite de resultados a retornar
+	 * @param string $order Campos para ordenação separados por vírgula
+	 * @return Device[] Dispositivos
+	 */
+	public static function getDevices($page, $limit, $order) {
+
+		// verifica se a página foi informada, se não for usa o padrão
+		if (!$page) {
+			$page = 1;
+		} else if (!is_int($page)) {
+			throw new \InvalidArgumentException("A página informada é inválida.");
+		}
+
+		//TODO ordenação
+
+		// verifica se a ordenação foi informada, se não for usa o padrão
+		if (!$order) {
+			//$order  = "userId, type, token";
+		} else if (!is_string($order)) {
+			throw new \InvalidArgumentException("A ordenação informada é inválida.");
+		}
+
+		// verifica se o limite foi informado e é válido
+		if (($limit && !is_int($limit))) {
+			throw new \InvalidArgumentException("O limite informado é inválido.");
+		}
+
+		global $entityManager;
+
+		$repository = $entityManager->getRepository(DeviceManager::DEVICE_REPOSITORY);
+
+		if ($limit) {
+			$offset = $limit * ($page - 1);
+			return $repository->findBy(array(), null, $limit, $offset);
+		} else {
+			return $repository->findAll(array());
 		}
 	}
 
 	/**
 	 * Busca os dispositivos com os tokens informados
 	 *
-	 * @param string[] $tokens
+	 * @param string[] $userIds Identificadores dos usuários aos quais o dispositivos deve pertencer
+	 * @return Device[] Dispositivos
 	 */
-	public static function getDevices($tokens) {
-		if (!$tokens || !is_array($tokens)) {
+	public static function getDevicesByUsers($userIds) {
+		if (!$userIds || !is_array($userIds)) {
+			throw new \InvalidArgumentException("O parãmetro informado é inválido.");
+		}
+
+		global $entityManager;
+
+		return $entityManager->getRepository(DeviceManager::DEVICE_REPOSITORY)->findBy(array("userId" => $userIds));
+	}
+
+	/**
+	 * Busca os dispositivos cadastrados de um usuário
+	 *
+	 * @param string $userId Identificador do usuário
+	 * @return Devices[] Dispositivos
+	 */
+	public static function getDevicesByUserId($userId) {
+		if (!$userId || !is_string($userId)) {
+			throw new \InvalidArgumentException("O parãmetro informado é inválido.");
+		}
+
+		global $entityManager;
+
+		return $entityManager->getRepository(DeviceManager::DEVICE_REPOSITORY)
+								->findBy(array("userId" => $userId), array("type" => "ASC"));
+	}
+
+	/**
+	 * Busca um dispositivo por seu identificador
+	 *
+	 * @param string $token Identificador do dispositivo
+	 * @return Device Dispositivo
+	 */
+	public static function getDevice($token) {
+		if (!$token || !is_string($token)) {
 			return null;
 		}
 
 		global $entityManager;
 
-		return $entityManager->getRepository(DeviceManager::DEVICE_REPOSITORY)->findBy(array("token" => $tokens));
+		return $entityManager->getRepository(DeviceManager::DEVICE_REPOSITORY)->findOneBy(array("token" => $token));
 	}
 
 	/**
@@ -88,7 +166,7 @@ class DeviceManager {
 	 * @return boolean True se atualizou um dispositivo, false caso contrário
 	 */
 	public static function updateDeviceToken($oldToken, $newToken) {
-		if (!$oldToken || !$newToken) {
+		if (!$oldToken || !is_string($oldToken) || !$newToken || !is_string($newToken)) {
 			return false;
 		}
 
@@ -118,7 +196,7 @@ class DeviceManager {
 	 * @return boolean True se removeu um dispositivo, false caso contrário
 	 */
 	public static function deleteDevice($token) {
-		if (!$token) {
+		if (!$token || !is_string($token)) {
 			return false;
 		}
 
@@ -142,6 +220,10 @@ class DeviceManager {
 	 *        	Identificador de push do dispositivo
 	 */
 	public static function exists($token) {
+		if (!$token || !is_string($token)) {
+			throw new \InvalidArgumentException("O identificador do dispositivo informado é inválido");
+		}
+
 		global $entityManager;
 
 		$queryBuilder = $entityManager->createQueryBuilder();
